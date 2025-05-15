a5daf40b58a113ae73f0ea67f5967ced1b48cefb
danielfranchi555 <danielfranchi3@gmail.com>
2025-05-15 00:27:59 -0300

    commit

--- src/app/components/WhatsappButton/WhatsappButton.tsx
+++ src/app/components/WhatsappButton/WhatsappButton.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/5493510000000" // Reemplaza con tu número
+      href="https://wa.me/5493510000000?text=Buongiorno%2C%20vorrei%20prenotare%20una%20lezione%20di%20padel"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300"
